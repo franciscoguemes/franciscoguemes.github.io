9615f9aefb26b3245ad6617b3707d032f7af2bd2
Francisco Güemes <francisco.gueemes@uniscon.net>
2019-12-17 12:44:11 +0100

    Form that send the email in PHP.

--- send_message.php
+++ send_message.php
@@ -1,4 +1,67 @@
 <?php
+
+// define variables and set to empty values
+$name = $email = $subject = $message = "";
+$valid_data = TRUE;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $subject = test_input($_POST["subject"]);
+  $message = test_input($_POST["message"]);
+
+  if( empty($name) || empty($email) || empty($subject) || empty($message)){
+      echo "There are empty fields. Please ensure that there are no empty fields in the form." . "<br>";
+      echo "The supplied values are:" . "<br>";
+      echo "Name: " . $email . "<br>";
+      echo "Email: " . $email . "<br>";
+      echo "Subject: " . $subject . "<br>";
+      echo "Message: " . $message . "<br>";
+      echo "<br>";
+      $valid_data = FALSE;
+  }
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+      echo "The supplied email:" . $email . " is not valid!!!";
+      echo "<br>";
+      $valid_data = FALSE;
+  }
+
+  if( $valid_data ){
+
+    $headers = 'From:' . $email . "\r\n" .
+    'Reply-To:' . $email . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+    $email_message = $name . " is sending you a message through the contact form at franciscoguemes.com" . "\r\n" .
+    "Here is the message:" . "\r\n" . "\r\n" . $message;
+
+    $outcome = mail("francisco@franciscoguemes.com",$subject,$email_message,$headers);
+    // mail("francisco.jose.guemes.sevilla@gmail.com",$subject,$message);
+
+    if($outcome){
+        echo "Your message has been successfully sent";
+        echo "<br>";
+    }else{
+        echo "There was a problem and the message was not sent. Please try again!";
+        echo "<br>";
+    }
+  }
+
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+
+
+
+/*
+
 // the message
 $msg = "This is a test!!! First line of text\nSecond line of text";
 
@@ -8,4 +71,11 @@ $msg = wordwrap($msg,70);
 // send email
 mail("francisco@franciscoguemes.com","My subject",$msg);
 mail("francisco.jose.guemes.sevilla@gmail.com","My subject",$msg);
+
+*/
+
 ?>
+
+
+
+
